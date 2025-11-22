0950595d9977a83aedd9b40c573e535171e1335c
KHO11 <54800683+KHO11@users.noreply.github.com>
2025-11-22 10:32:12 +0000

    remove changes

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,5 @@ import java.util.Scanner;
 
 void main() {
     
-    // extract a substring
-    String name = "kevin hyde-opoku";
-    String firstName = name.substring(0, 5);   // kevin
-    IO.println(firstName);
-
-    // validate user commands
-    Scanner scanner = new Scanner(System.in);
-
-    System.out.print("Enter your age: ");
-    int age = scanner.nextInt();
-
-    // Create category: 1 = under 18, 2 = 18 or above
-    int category = (age >= 18) ? 2 : 1;
-
-    switch (category) {
-        case 1:
-            System.out.println("You are under 18.");
-            break;
-
-        case 2:
-            System.out.println("You are 18 or above.");
-            break;
-        }
-
-        scanner.close();
-
-        // convert a name to upper case
-        IO.println(name.toUpperCase());
+  
 }
